b5072ddd90666769652920019526aa5e275a4aa5
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2024-04-28 12:30:20 -0300

    Basic types from Elixir

---
+++ basic_types.exs
@@ -0,0 +1,17 @@
+1          # integer
+0x1F       # integer
+1.0        # float
+true       # boolean
+:atom      # atom / symbol
+"elixir"   # string
+[1, 2, 3]  # list
+{1, 2, 3}  # tuple
+
+#=================== BASIC ARITHMETIC
+1 + 2 = 3
+5 * 5 = 25
+
+10 / 2 = 5.0
+div(10, 2) = 5
+div 10, 2 = 5
+rem 10, 3 = 1
